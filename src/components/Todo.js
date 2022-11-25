191d393663cd536168faaced3a1748c14d1ae042
EricSoklowski <ericsoko2002@gmail.com>
2022-11-25 12:38:15 -0500

    add deleteHandler shell to button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,14 @@
 function Todo(props){
+
+  function deleteHandler() {
+
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={deleteHandler}>DELETE</button>
       </div>
     </div>
   )
